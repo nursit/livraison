33242346b065fc0c02bbd0449d0333b11a3c8418
Cerdic <cedric@yterium.com>
2015-07-20 16:35:44 +0200

    Generer un CSV a la desinstallation, et importer le csv fournit par le plugin a l'installation

--- livraison_administrations.php
+++ livraison_administrations.php
@@ -24,8 +24,14 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function livraison_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
-	$maj['create'] = array(array('maj_tables', array('spip_livraisonmodes')));
-	$maj['1.0.1'] = array(array('maj_tables', array('spip_livraisonmodes')));
+	$maj['create'] = array(
+		array('maj_tables', array('spip_livraisonmodes')),
+		array('livraison_installer_modes'),
+	);
+
+	$maj['1.0.1'] = array(
+		array('maj_tables', array('spip_livraisonmodes')),
+	);
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -40,6 +46,7 @@ function livraison_upgrade($nom_meta_base_version, $version_cible) {
  * @return void
 **/
 function livraison_vider_tables($nom_meta_base_version) {
+	livraison_generer_csv_installation();
 
 	sql_drop_table("spip_livraisonmodes");
 
@@ -51,4 +58,29 @@ function livraison_vider_tables($nom_meta_base_version) {
 	effacer_meta($nom_meta_base_version);
 }
 
-?>
+function livraison_installer_modes(){
+	include_spip("action/editer_objet");
+	if ($importer_csv = charger_fonction("importer_csv","inc",true)
+	  AND $f = find_in_path("base/livraisonmodes.csv")){
+		$modes = $importer_csv($f,true);
+
+		foreach($modes as $mode){
+			$id = objet_inserer("livraisonmode");
+			$set = $mode;
+			$set['prix_poids_ht'] = preg_replace(",\s+,","\n",$set['prix_poids_ht']);
+			$set['prix_volume_ht'] = preg_replace(",\s+,","\n",$set['prix_volume_ht']);
+			$set['statut'] = 'prop';
+			objet_modifier("livraisonmode",$id,$set);
+		}
+	}
+}
+
+function livraison_generer_csv_installation(){
+	if ($exporter_csv = charger_fonction("exporter_csv","inc",true)){
+		$champs = array(
+			"titre","descriptif","zone_pays","zone_cp","zone_cp_exclus","taxe","prix_forfait_ht","prix_unit_ht","prix_poids_ht","prix_volume_ht"
+		);
+		$modes = sql_allfetsel(implode(",",$champs),"spip_livraisonmodes","statut='publie'");
+		$exporter_csv("livraisonmodes",$modes,",",$champs,false);
+	}
+}
